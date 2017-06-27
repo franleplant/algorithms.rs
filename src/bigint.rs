297dcbb4a05b8ec450883cff5824ad4800328ed0
Fran Guijarro <franleplant@gmail.com>
2017-06-27 16:16:03 -0300

    better bigint sum

--- src/bigint.rs
+++ src/bigint.rs
@@ -8,10 +8,19 @@ struct BigInt {
 
 
 impl BigInt {
-    fn new(number: Vec<usize>) -> BigInt {
+    pub fn new(number: Vec<usize>) -> BigInt {
         BigInt {
-            number: number.iter().cloned().rev().collect(),
+            number: number,
         }
+    }
+
+    pub fn get_digit(&self, index: usize) -> usize {
+        let len = self.number.len();
+        if index > len - 1 {
+            return 0;
+        }
+
+        self.number[len - index - 1]
     }
 }
 
@@ -19,20 +28,19 @@ impl BigInt {
 impl Add for BigInt {
     type Output = BigInt;
     fn add(self, rhs: BigInt) -> BigInt {
-        let mut numbers = vec![];
-        let mut i = 0;
-        let mut carry = 0;
-        let z = 0;
         let self_len = self.number.len();
         let other_len = rhs.number.len();
+
+        let mut i = 0;
+        let mut carry = 0;
+        let mut numbers = vec![];
         loop {
-            if i >= self_len && i >= other_len {
+            if i > self_len && i > other_len {
                 break;
             }
 
-
-            let a = self.number.get(i).unwrap_or(&z);
-            let b = rhs.number.get(i).unwrap_or(&z);
+            let a = self.get_digit(i);
+            let b = rhs.get_digit(i);
 
             let mut sum = a + b + carry;
             if sum >= 10 {
@@ -55,11 +63,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let n1 = BigInt::new(vec![1,9]);
-        let n2 = BigInt::new(vec![1]);
+    fn add() {
+        let n1 = BigInt::new(vec![9,9]);
+        let n2 = BigInt::new(vec![1,0]);
         let n3 = n1 + n2;
 
-        assert_eq!(n3, BigInt::new(vec![2, 0]))
+        assert_eq!(n3, BigInt::new(vec![1, 0, 9]))
     }
 }
